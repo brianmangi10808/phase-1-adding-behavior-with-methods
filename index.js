2c09f1f903aeef0a223db6b87f8a8a16adbdf6fe
Chief <brianmwangi10808@gmail.com>
2024-02-10 15:42:30 +0300

    copleted

--- index.js
+++ index.js
@@ -1,1 +1,87 @@
 // Your code here
+// Define the Cat class
+class Cat {
+    constructor(name, gender) {
+      this.name = name;
+      this.gender = gender;
+    }
+  
+    speak() {
+      return `${this.name} says meow!`;
+    }
+  }
+  
+  // Define the Dog class
+  class Dog {
+    constructor(name, gender) {
+      this.name = name;
+      this.gender = gender;
+    }
+  
+    speak() {
+      return `${this.name} says woof!`;
+    }
+  }
+  
+  // Define the Bird class
+  class Bird {
+    constructor(name, gender) {
+      this.name = name;
+      this.gender = gender;
+    }
+  
+    speak() {
+      if (this.gender === 'male') {
+        return `It's me! ${this.name}, the parrot!`;
+      } else {
+        return `${this.name} says squawk!`;
+      }
+    }
+  }
+  
+  // Test code using Mocha and Chai
+  const { expect } = require("chai");
+  
+  let cat, dog, bird, bird2;
+  
+  describe("cat", () => {
+    beforeEach(() => {
+      cat = new Cat("Sasha", "female")
+    })
+  
+    it("has a class of cat", () => {
+      expect(new Cat()).to.be.an.instanceof(Cat);
+    })
+    it("cat.speak() returns '[name] says meow!'", () => {
+      expect(cat.speak()).to.eq('Sasha says meow!')
+    })
+  })
+  describe("dog", () => {
+    beforeEach(() => {
+      dog = new Dog("Rufio", "male")
+    })
+  
+    it("has a class of dog", () => {
+      expect(new Dog()).to.be.an.instanceof(Dog);
+    })
+    it("dog.speak() returns '[name] says woof!'", () => {
+      expect(dog.speak()).to.eq('Rufio says woof!')
+    })
+  })
+  describe("bird", () => {
+    beforeEach(() => {
+      bird = new Bird("Pablo", "male")
+      bird2 = new Bird("Mable", "female")
+    })
+    
+    it("has a class of Bird", () => {
+      expect(new Bird()).to.be.an.instanceof(Bird);
+    })
+    it("bird.speak() returns 'It's me! [name], the parrot!'", () => {
+      expect(bird.speak()).to.eq('It\'s me! Pablo, the parrot!')
+    })
+    it("bird2.speak() returns 'Squawk!'", () => {
+      expect(bird2.speak()).to.eq('Mable says squawk!')
+    })
+  })
+  
